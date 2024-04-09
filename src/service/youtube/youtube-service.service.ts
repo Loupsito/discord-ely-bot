4826c03035203cf6ce2a1b0c8c58439ce726d185
AlexandreS <alexandre.suy@gmail.com>
2024-04-09 19:07:41 +0200

    update label duration

--- src/service/youtube/youtube-service.service.ts
+++ src/service/youtube/youtube-service.service.ts
@@ -43,6 +43,6 @@ export class YoutubeService {
   private toMinutesAndSeconds(duration: number): string {
     const minutes = Math.floor(duration / 60);
     const seconds = duration - minutes * 60;
-    return `${minutes}:${seconds}`;
+    return `${minutes} min : ${seconds} sec`;
   }
 }
