034ecf2e2976547291b1662bdd8469485ae69bdf
AlexandreS <alexandre.suy@gmail.com>
2024-04-09 19:07:51 +0200

    update yt cookie

--- src/service/youtube/youtube-service.service.ts
+++ src/service/youtube/youtube-service.service.ts
@@ -24,7 +24,7 @@ export class YoutubeService {
     }
   }
   getStream(url: string) {
-    const COOKIE = `YSC=90QeZTohnO4; VISITOR_PRIVACY_METADATA=CgJGUhIIEgQSAgsMIC4%3D; PREF=f6=40000000&tz=Europe.Paris; SOCS=CAISEwgDEgk2MjE0MDk3NTcaAmZyIAEaBgiA_rywBg; VISITOR_INFO1_LIVE=BHr_3SggUVY; GPS=1`;
+    const COOKIE = `YSC=uhRO9TnQXgY; VISITOR_PRIVACY_METADATA=CgJGUhIIEgQSAgsMIGU%3D; PREF=f6=40000000&tz=Europe.Paris&f7=100; SOCS=CAISEwgDEgk2MjI2ODk2MDcaAmZyIAEaBgiAitKwBg; VISITOR_INFO1_LIVE=bEju-IBcwos; GPS=1; CONSISTENCY=AKreu9uZYHypKuGiYgGZZDX52vhU6-46FgtgM8mXKX_Bx6OFUzW4tOYnWhfkrbYAR-luZkEcVbLA3hrlcGjCkH3infAbUvwjzx0uBBa9DD3U4DhHs-5bpRLvIlmaNGQHJluX7eMou5NU2vBhPXo2g5M`;
     return ytdl(url, {
       filter: 'audioonly',
       highWaterMark: 32 * 1024 * 1024,
